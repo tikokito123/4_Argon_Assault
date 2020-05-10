af73badadf3d6dc376df759ceb3b3485e243d7ee
Tikokito <tikokito123@gmail.com>
2020-05-11 01:02:16 +0300

    added a splash scene

---
+++ 4_Argon_Assault/Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 5.8f);
+        DontDestroyOnLoad(transform.gameObject);
+    }
+   void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
