7e6554b870f235df7430c716a9a29868028898e3
Tikokito <tikokito123@gmail.com>
2020-05-11 21:09:01 +0300

    the spaceship is moving

---
+++ 4_Argon_Assault/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("ms^-1")][SerializeField] float xSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        rawNewXPos = Mathf.Clamp(rawNewXPos, -4.5f, 4.5f);
+        transform.localPosition = new Vector3(rawNewXPos , transform.localPosition.y, transform.localPosition.z);
+        
+    }
+}
