f8550acad5a3f326d97bd04c1f3075feb07fd720
Tikokito <tikokito123@gmail.com>
2020-05-11 23:25:26 +0300

    The spaceship is moving up and down

--- 4_Argon_Assault/Assets/Player.cs
+++ 4_Argon_Assault/Assets/Player.cs
@@ -5,7 +5,7 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("ms^-1")] [SerializeField] float speed = 4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +16,14 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        rawYPos = Mathf.Clamp(rawYPos, -3f, 3f);
         float rawNewXPos = transform.localPosition.x + xOffset;
         rawNewXPos = Mathf.Clamp(rawNewXPos, -4.5f, 4.5f);
-        transform.localPosition = new Vector3(rawNewXPos , transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(rawNewXPos , rawYPos, transform.localPosition.z);
         
     }
 }
