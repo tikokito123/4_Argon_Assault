c8dc32266e40c2aa40c393953eb6c526725539f3
Tikokito <tikokito123@gmail.com>
2020-05-11 04:21:15 +0300

    import standard assets

--- 4_Argon_Assault/Assets/MusicPlayer.cs
+++ 4_Argon_Assault/Assets/MusicPlayer.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
         Invoke("LoadFirstScene", 5.8f);
-        DontDestroyOnLoad(transform.gameObject);
     }
    void LoadFirstScene()
     {
